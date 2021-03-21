66a3c0baac161c6c2619e5212c40685e475fbfe6
Lukas Krauch <lukas.krauch@yahoo.de>
2021-03-21 12:50:04 +0100

    Add setting page item

---
+++ lib/src/setting_page_item.dart
@@ -0,0 +1,43 @@
+import 'package:clean_settings/clean_settings.dart';
+import 'package:flutter/material.dart';
+
+import 'setting_styles.dart';
+
+class SettingPageItem extends StatelessWidget {
+  final String title;
+  final TextStyle titleStyle;
+  final String displayValue;
+  final TextStyle displayValueStyle;
+  final GestureTapCallback onTap;
+  final ItemPriority priority;
+
+  const SettingPageItem({
+    Key key,
+    @required this.title,
+    this.titleStyle,
+    this.displayValue,
+    this.displayValueStyle,
+    @required this.onTap,
+    this.priority = ItemPriority.normal,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final listTile = ListTile(
+      dense: true,
+      visualDensity: VisualDensity.comfortable,
+      contentPadding: const EdgeInsets.symmetric(horizontal: 15.0),
+      title: Text(title,
+          style: titleStyle ?? kGetDefaultTitleStyle(context, priority)),
+      trailing: const Icon(Icons.chevron_right),
+      subtitle: displayValue != null
+          ? Text(displayValue,
+              style: displayValueStyle ??
+                  kGetDefaultSubTitleStyle(context, priority))
+          : null,
+    );
+    return priority == ItemPriority.disabled
+        ? listTile
+        : InkWell(onTap: onTap ?? () {}, child: listTile);
+  }
+}
